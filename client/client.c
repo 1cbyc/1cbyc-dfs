7a07cc0cd2722e91376e0e9a81d2ef4d9f4a065a
1cbyc <isaacnsisong@gmail.com>
2024-12-27 12:42:36 +0100

    Changes made on 2024-12-27 12:42:36

--- client/client.c
+++ client/client.c
@@ -9,6 +9,7 @@
 #define FILENAME "sample.txt" // in my quest to add file upload
 
 int send_file(int sock, const char *filename) {
+int
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         perror("File not found");
