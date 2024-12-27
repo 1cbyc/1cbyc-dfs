fc77f1531383f1a62b7c59982fca3beccf86e7d3
1cbyc <isaacnsisong@gmail.com>
2024-12-27 12:46:10 +0100

    Changes made on 2024-12-27 12:46:10

--- client/client.c
+++ client/client.c
@@ -19,7 +19,7 @@ int download_file(int sock, const char *filename) {
     ssize_t bytes_received;
 
     // requesting the file from the server
-    send(sock, filename, strlen(filename))
+    send(sock, filename, strlen(filename), 0)
 
 }
 
