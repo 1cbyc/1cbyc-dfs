0c92b9430ba459c3407e4a8ea8191f9aada5d9ec
1cbyc <isaacnsisong@gmail.com>
2024-12-27 12:16:58 +0100

    Changes made on 2024-12-27 12:16:58

--- server/server.c
+++ server/server.c
@@ -21,7 +21,7 @@ void handle_client(int client_sock) {
     ssize_t bytes_received;
     // to receive data from client side
     while ((bytes_received = recv(client_sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
-        fw
+        fwrite(buffer, 1, bytes_received)
         buffer[bytes_received] = '\0';
         printf("Received: %s\n", buffer);
         
